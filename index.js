0aae46af079c50af4debde56c57f5932e3ff2f6f
Nathan de Farias Melo <111164902+FMNathan@users.noreply.github.com>
2024-02-12 17:10:41 -0300

    adicionando a funcionalidade de Desencriptografar texto

--- index.js
+++ index.js
@@ -5,11 +5,11 @@
         
 
         let textoCripto = texto
-        .replace(/e/gi, "enter")
-        .replace(/i/gi, "imes")
-        .replace(/a/gi, "ai")
-        .replace(/o/gi, "ober")
-        .replace(/u/gi, "ufat")
+            .replace(/e/gi, "enter")
+            .replace(/i/gi, "imes")
+            .replace(/a/gi, "ai")
+            .replace(/o/gi, "ober")
+            .replace(/u/gi, "ufat")
         
         if (texto.length != 0){
             document.getElementById("texto").value = textoCripto
@@ -21,3 +21,26 @@
             alert("Deve inserir algum texto")
         }
     } 
+
+    function descriptografar(){
+        let texto = document.getElementById("texto").value
+        let tituloMensagem = document.getElementById("titulo-mensagem")
+        let paragrafo = document.getElementById("paragrafo-mensagem")
+        
+        let textoCripto = texto 
+            .replace(/enter/gi, "e")
+            .replace(/imes/gi, "i")
+            .replace(/ai/gi, "a")
+            .replace(/ober/gi, "o")
+            .replace(/ufat/gi, "u")
+
+            if (texto.length != 0){
+                document.getElementById("texto").value = textoCripto
+                tituloMensagem.textContent = "Texto descriptografado com sucesso!"
+                paragrafo.textContent = ""
+            } else{
+                tituloMensagem.textContent = "Nenhuma mensagem encontrada"
+                paragrafo.textContent = "Digite um texto que você deseja criptografar ou descriptografar."
+                alert("Deve inserir algum texto")  
+            }
+    }   
